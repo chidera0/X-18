2b316a835b31590e80f6c95307ce9bba3f4d09f2
aliengod16 <62847147+aliengod16@users.noreply.github.com>
2020-04-19 06:20:05 -0700

    Add files via upload

---
+++ domain.py
@@ -0,0 +1,31 @@
+# import pyfiglet module 
+import pyfiglet 
+
+from colorama import Fore, init 
+import requests
+init()
+R=pyfiglet.figlet_format("DNSCAN ",font = "slant")
+print(R)
+print(f"{Fore.RED}https://web.facebook.com/Alien-god-108932904033061", Fore.RESET)
+print(f"{Fore.RED}created by Aliengod16", Fore.RESET)
+print(f"{Fore.RED}https://www.youtube.com/channel/UCj9j_SE324s6_Z_CelSe2dQ", Fore.RESET)
+# the domain to scan for subdomains
+domain = input("[+]enter domain to scan: ")
+#open file subdomain
+domains = open("subdomain.txt")
+# read all the content of the file
+contents = domains.read()
+
+new_subdomains = contents.splitlines()
+
+for subdomain in new_subdomains:
+    # construct the url from new_subdomains:
+    link = f"http://{subdomain}.{domain}"
+    try:
+       
+        requests.get(link)
+    except requests.ConnectionError:
+        
+        pass
+    else:
+        print(f"{Fore.GREEN}[+]Found subdomain:", link , Fore.RESET)
